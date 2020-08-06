07e48a9fdb2e71fb76bec87710a9305b22c8c3c0
Soto Rodríguez <687238@cognizant.com>
2020-08-05 23:28:18 -0500

    Completed module

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -9,9 +9,13 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
+
   constructor(private _productService: ProductService) { }
 
   ngOnInit() {
+    this._productService.getAlbum(1).subscribe(response =>
+      this.albumInfo = response);
   }
 
 }
