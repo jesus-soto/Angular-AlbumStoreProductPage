0a1654ab39b33d21fe1ca7fde4f901f78247f904
Soto Rodríguez <687238@cognizant.com>
2020-08-07 02:47:58 -0500

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -26,8 +26,9 @@ export class ProductService {
       <Album>response.json());
   }
 
-  getProducts(){
-    return this._http.get(this._productsUrl).map((response) => <Product[]>response.json());
+  getProducts():Observable<Product[]>{
+    return this._http.get(this._productsUrl).map((response) =>
+      <Product[]>response.json());
   }
 
 
